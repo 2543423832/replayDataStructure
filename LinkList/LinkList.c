debdbb7e19544f34564aef7fc1f3ed70184bb1cd
azy <2543423832@qq.com>
2023-12-20 10:28:42 +0800

    [修改] 新增链表初始化

--- LinkList/LinkList.c
+++ LinkList/LinkList.c
@@ -1,68 +1,89 @@
 #include "LinkList.h"
+#include <stdlib.h>
+#include <string.h>
 
+enum STATUS_CODE // 枚举
+{
+    ON_SUCCESS,
+    NULL_PTR,
+    MALLOC_ERROR,
+    INVALID_ACCESS,
 
+};
 /*链表初始化*/
 int LinkListInit(LinkList **pList)
 {
+    int ret = 0;
+    LinkList *list = (LinkList *)malloc(sizeof(LinkList) * 1);
+    if (list == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+    /*清除脏数据*/
+    memset(list, 0, sizeof(LinkList) * 1);
+    list->head = malloc(sizeof(LinkList) * 1);
+    if (list->head == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+    /*清除脏数据*/
+    memset(list->head, 0, sizeof(LinkList) * 1);
+    list->head->data = 0;
+    list->head->next = NULL;
+    /*链表的长度为0*/
+    list->len = 0;
 
+    /*二级指针*/
+    *pList = list;
+    return ret;
 }
 
 /*链表头插*/
 int LinkListHeadInsert(LinkList *pList, ELEMENTTYPE val)
 {
-
 }
 
 /*链表尾插*/
 int LinkListTaillnsert(LinkList *pList, ELEMENTTYPE val)
 {
-
 }
 
 /*链表指定位置插入*/
 int LinkListAppointPosInsert(LinkList *pList, int pos, ELEMENTTYPE val)
 {
-
 }
 
 /*链表头删*/
 int LinkListHeadDel(LinkList *pList)
 {
-
 }
 
 /*链表尾删*/
 int LinkListTailDel(LinkList *pList)
 {
-
 }
 
 /*链表删除指定位置*/
 int LinkListDelAppointPos(LinkList *pList, int pos)
 {
-
 }
 
 /*链表删除指定数据*/
 int LinkListDelAppointData(LinkList *pList, ELEMENTTYPE val)
 {
-
 }
 
 /*获取链表的长度*/
 int LinkListGetLength(LinkList *pList, int *pSize)
 {
-
 }
 
 /*链表的销毁*/
 int LinkListDestroy(LinkList *pList)
 {
-
 }
 
 /*链表遍历接口*/
 int LinkListForeach(LinkList *pList)
 {
-    
 }
