e5bf4797bf622df7bd50d016b4fc73a78867a419
azy <2543423832@qq.com>
2023-12-20 11:26:36 +0800

    [修改]完善链表的遍历操作

--- LinkList/LinkList.c
+++ LinkList/LinkList.c
@@ -123,7 +123,7 @@ int LinkListGetLength(LinkList *pList, int *pSize)
     }
     if (pSize)
     {
-      *pSize = pList->len;
+        *pSize = pList->len;
     }
     return ret;
 }
@@ -136,5 +136,27 @@ int LinkListDestroy(LinkList *pList)
 /*链表遍历接口*/
 int LinkListForeach(LinkList *pList)
 {
+    if (pList == NULL)
+    {
+        return NULL_PTR;
+    }
+#if 0
+    /*从虚拟头结点开始遍历*/
+    LinkNode *travelNode = pList->head;
+     while (travelNode->next != NULL)
+    {
+        travelNode = travelNode->next;
+        printf("travelNode->data:%d\n",travelNode->data);
+    }
+#else
+    LinkNode *travelNode = pList->head->next;
+      while (travelNode->next != NULL)
+    {
+        
+        printf("travelNode->data:%d\n",travelNode->data);
+        travelNode = travelNode->next;
+    }
     
+#endif
+   
 }
